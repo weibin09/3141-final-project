ee90d7d3ccc1aad56d195a983cb555628fde215e
weibin ZHANG <benykarenz@gmail.com>
2021-12-03 18:18:19 -0500

    Add files via upload

--- weather.js
+++ weather.js
@@ -5,8 +5,36 @@ let weather ={
         .then((response)=>response.json())
         // .then((data)=>console.log(data));
         .then((data)=>this.displayWeather(data));
+    
     },
 
+    // displayWeatherc:function (data) {
+    //    const {name} = data;
+    //    const {icon, description} = data.weather[0];
+    //     const {temp, humidity,feels_like,temp_max,temp_min}= data.main;
+    //     const {speed} = data.wind;
+    //     const {country,sunrise,sunset} = data.sys;
+    //     console.log(name, icon,description,temp,humidity,speed,temp_max);
+    //     let ct=(temp-32)/1.8;
+    //     let cf=(feels_like-32)/1.8;
+    //     let ctemp_max=(temp_max-32)/1.8;
+    //     let ctemp_min=(temp_min-32)/1.8;
+    //     document.querySelector(".city").innerText = "Weather in "+ name;
+    //     document.querySelector(".icon").src="https://openweathermap.org/img/wn/"+icon +".png";
+    //     document.querySelector(".description").innerText = description;
+    //     document.querySelector(".temp").innerText =  ct +"°C";
+    //     document.querySelector(".humidity").innerText = "humidity: "+ humidity+"%";
+    //     document.querySelector(".feels_like").innerText = "feels like: "+cf+"°C";
+    //     document.querySelector(".temp_max").innerText = "H : "+ctemp_max+"°C ";
+    //     document.querySelector(".temp_min").innerText = "L : "+ctemp_min+"°C";
+    //     document.querySelector(".wind").innerText = "Wind speed: "+ speed+"km/h";
+    //     // document.querySelector("")
+    //     console.log(country+","+sunrise+","+sunset);
+    //     document.querySelector(".weather").classList.remove("loading");
+       
+
+
+    // },
     
     displayWeather: function(data){
         const {name} = data;
@@ -15,19 +43,11 @@ let weather ={
         const {speed} = data.wind;
         // const {rain1} =data.rain;
         const {country,sunrise,sunset} = data.sys;
-
         console.log(name, icon,description,temp,humidity,speed,temp_max);
         document.querySelector(".city").innerText = "Weather in "+ name;
         document.querySelector(".icon").src="https://openweathermap.org/img/wn/"+icon +".png";
         document.querySelector(".description").innerText = description;
-        
-        
         document.querySelector(".temp").innerText =  temp +"°F";
-        
-
-        
-
-        
         document.querySelector(".humidity").innerText = "humidity: "+ humidity+"%";
         document.querySelector(".feels_like").innerText = "feels like: "+feels_like+"°F";
         document.querySelector(".temp_max").innerText = "H : "+temp_max+"°F ";
@@ -36,20 +56,21 @@ let weather ={
         // document.querySelector("")
         console.log(country+","+sunrise+","+sunset);
         document.querySelector(".weather").classList.remove("loading");
-        let ct=(temp-32)/1.8;
-        let cf=(feels_like-32)/1.8;
-        let ctemp_max=(temp_max-32)/1.8;
-        let ctemp_min=(temp_min-32)/1.8;
+        let ct=Math.floor((temp-32)/1.8);
+        let cf=Math.floor((feels_like-32)/1.8);
+        let ctemp_max=Math.floor((temp_max-32)/1.8);
+        let ctemp_min=Math.floor((temp_min-32)/1.8);
         // document.body.style.backgroundImage="url('https://source.unsplash.com/1600*900/?"+ name +"')";
+        // document.body.style.backgroundImage="url"(' https://api.unsplash.com/search/photos?query=canada ');
         //-----------------------------------------------------------------------------------------------
-        // document.querySelector(".temp").addEventListener("click",function(){
-        //     document.querySelector(".temp").innerText =  ct +"°C";
-        //     document.querySelector(".humidity").innerText = "humidity: "+ humidity+"%";
-        //     document.querySelector(".feels_like").innerText = "feels like: "+cf+"°C";
-        //     document.querySelector(".temp_max").innerText = "H : "+ctemp_max+"°C ";
-        //     document.querySelector(".temp_min").innerText = "L : "+ctemp_min+"°C";
-        //     document.querySelector(".wind").innerText = "Wind speed: "+ speed+"km/h";
-        // });
+        document.querySelector(".temp").addEventListener("click",function(){
+            document.querySelector(".temp").innerText =  ct +"°C";
+            document.querySelector(".humidity").innerText = "humidity: "+ humidity+"%";
+            document.querySelector(".feels_like").innerText = "feels like: "+cf+"°C";
+            document.querySelector(".temp_max").innerText = "H : "+ctemp_max+"°C ";
+            document.querySelector(".temp_min").innerText = "L : "+ctemp_min+"°C";
+            document.querySelector(".wind").innerText = "Wind speed: "+ speed+"km/h";
+        });
         
         // console.log(country+","+sunrise+","+sunset);
         // document.querySelector(".weather").classList.remove("loading");
@@ -85,6 +106,10 @@ document.querySelector(".search_bar").addEventListener("keyup",function(event){
 });
 
 weather.fetchWeather("brooklyn");
+//=========================================
+document.querySelector(".convert").addEventListener("click", function name(params) {
+    displayWeatherc();
+});
 
 const d = new Date();
 let country = document.querySelector(".search_bar").value;
